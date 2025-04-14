2a527e0ed7ee1fcf1f82c1513b6a0fa3d1d8b0f8
ed-matheus <edsonmatheus02@hotmail.com>
2025-04-14 17:10:30 -0300

    primeira seção da homepage finalizada, junto com alterações no Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Importando FontAwesome
+
+// Ícones do FontAwesome
+import { faCode, faPenRuler, faUsers } from "@fortawesome/free-solid-svg-icons";
+
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-space-mono)]">
-      <h1 className="text-blue-500">Teste</h1>
-    </div>
-  );
+	return (
+		<div className="items-center justify-items-center">
+			<section className="flex justify-center items-center w-full px-5 py-10 bg-black-400">
+				<div className="w-[60%]">
+					<h2 className="text-blue-500 mb-5 text-2xl">
+						Olá, me chamo Matheus <br /> Designer de Interfaces e Desenvolvedor
+						Front-end
+					</h2>
+
+					<p className="text-xl">
+						Transformo ideias em experiências digitais modernas, funcionais e
+						intuitivas. <br />
+						Este é o meu portfólio, onde compartilho meus projetos, habilidades
+						e visão criativa.
+					</p>
+
+					<ul className="flex gap-18 mt-6">
+						<li className="flex items-center text-[1.2rem]">
+							<FontAwesomeIcon icon={faPenRuler} className="me-2 w-[1.3rem]" />
+							Web Design
+						</li>
+						<li className="flex items-center text-[1.2rem]">
+							<FontAwesomeIcon icon={faUsers} className="me-2 w-[1.3rem]" />
+							UX/UI Design
+						</li>
+						<li className="flex items-center text-[1.2rem]">
+							<FontAwesomeIcon icon={faCode} className="me-2 w-[1.3rem]" />
+							Desenvolvimento Front-end
+						</li>
+					</ul>
+				</div>
+
+				<img src="/me.png" alt="" className="w-[25rem] h-[25rem]" />
+			</section>
+		</div>
+	);
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,26 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   // Lista com path e nome de cada item do menu (navegação)
   const menuItems = [
-    {"id": 0, "path": "/sobre", "name": "Sobre"},
-    {"id": 1, "path": "/habilidades", "name": "Habilidades"},
-    {"id": 2, "path": "/projetos", "name": "Projetos"},
-    {"id": 3, "path": "/contato", "name": "Contato"},
-  ]
+    { id: 0, path: "/sobre", name: "Sobre" },
+    { id: 1, path: "/habilidades", name: "Habilidades" },
+    { id: 2, path: "/projetos", name: "Projetos" },
+    { id: 3, path: "/contato", name: "Contato" },
+  ];
 
   return (
-    <header className='flex justify-between items-center px-8 py-6 text-light bg-black-500'>
-      <Link href={"/"} className='text-blue-500 text-xl'>
+    <header className="flex justify-between items-center px-18 py-7 text-light bg-black-400 border-b-2 border-blue-500">
+      <Link href={"/"} className="text-blue-500 text-xl">
         Matheus Costa
       </Link>
+
       <nav>
-        <ul className='flex gap-8'>
+        <ul className="flex gap-15">
           {menuItems.map((item) => (
             <li key={item.id}>
-              <Link href={`${item.path}`} className='hover:text-blue-500'>
+              <Link href={`${item.path}`} className="hover:text-blue-500">
                 {item.name}
               </Link>
             </li>
@@ -27,7 +28,7 @@ const Header = () => {
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
