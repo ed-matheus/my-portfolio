5706b8cc7a2ed9b3a06159adb2f741d868896e78
ed-matheus <edsonmatheus02@hotmail.com>
2025-10-05 22:36:43 -0300

    melhorando o rodapé do site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Importando FontAwesome
 
 // Ícones do FontAwesome
@@ -6,32 +6,34 @@ import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="w-full py-5 px-8 bg-cyan flex justify-between items-center">
-      <p className="text-sm font-medium text-light">
-        Site desenvolvido totalmente em código com Next.js, pelo próprio{" "}
-        <span className="text-dark font-bold">Edson Matheus</span>
+    <footer className="w-full p-8 bg-light flex justify-around items-center text-dark">
+      <p className="text-sm md:text-lg font-medium">
+        © 2025 Edson Matheus. Todos os direitos reservados.
       </p>
 
       {/* Redes Sociais */}
-      <div className="flex items-center gap-4 ms-5">
+      <div className="flex items-center gap-4 ms-5 text-light">
         <a
           href="https://www.linkedin.com/in/edson-matheus-b5a0171ba/"
           target="__blank"
         >
           <button
             type="button"
-            className="bg-dark-500 p-2 rounded-full hover:cursor-pointer"
+            className="bg-dark p-2 rounded-full cursor-pointer"
           >
-            <FontAwesomeIcon icon={faLinkedinIn} className="w-[1rem]" />
+            <FontAwesomeIcon
+              icon={faLinkedinIn}
+              className="w-[1rem] p-1 text-md"
+            />
           </button>
         </a>
 
         <a href="https://github.com/ed-matheus" target="__blank">
           <button
             type="button"
-            className="bg-dark-500 p-2 rounded-full hover:cursor-pointer"
+            className="bg-dark p-2 rounded-full cursor-pointer"
           >
-            <FontAwesomeIcon icon={faGithub} className="w-[1rem]" />
+            <FontAwesomeIcon icon={faGithub} className="w-[1rem] p-1" />
           </button>
         </a>
       </div>
